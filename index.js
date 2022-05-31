a6e933237782ddeb9407b00e3150ab19736f5fed
aicha <soumaima@hotmail.fr>
2022-05-31 11:47:34 +0200

    create new branche with update server and pull last update dev

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const data = require('./data.json');
 const axios = require('axios');
-//const cors = require('cors');
+const cors = require('cors');
 const port = 5050;
 const app = express();
 
@@ -9,7 +9,61 @@ app.use(express.urlencoded({
     extended: true
 }));
 
+app.get("/", (req, res) => {
+    res.send("hello la team!").status(200);
+});
 
+app.get("/api/items", (req, res) => {
+    res.send(data).status(200);
+});
+
+app.get("/api/items/:id", (req, res) => {
+    const id = req.params.id;
+    const item = data.find(element => element.id == id);
+    if(item) res.send(item).status(200);
+    else res.send("item not found").status(404);    
+});
+
+app.post("/api/items", (req, res) => {
+    if (!req.body.title || !req.body.category || !req.body.name){
+        res.send("Missing fields").status(404);
+    } else {
+    const item = {
+        id: data.length + 1,
+        title: req.body.title,
+        category: req.body.catagory,
+        name: req.body.name
+    };
+    data.push(item);
+    res.send ({message:"item successfully created", item: item}).status(200);
+    }
+});
+
+app.patch("/api/items/:id", (req, res) => {
+    const id = req.params.id;
+    const item = data.find(element => element.id == id);
+    if(item){
+        if (req.body.title) item.title = req.body.title;
+        if (req.body.name) item.name = req.body.name;
+        if (req.body.category) item.category = req.body.category;
+        res.send ({message:"item successfully edited", item: item}).status(200);
+
+    } else {
+        res.send("Item not found").status(404);
+    }      
+});
+
+app.delete("/api/items/:id", (req, res) => {
+    const id = req.params.id;
+    const item = data.find(element => element.id == id);
+    if(item){
+        data.splice(data.indexOf(item), 1);
+        res.send  ({message:"item successfully deleted", item: item}).status(200);
+    }else{
+        res.send("Item not found").status(404);
+    }      
+});
+     
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
